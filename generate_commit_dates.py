9ebe1c6f18aa280d065b35180dc7a857a7b43306
WangZhen <wangzhenjjcn@gmail.com>
2023-06-06 03:10:24 +0800

    Update generate_commit_dates.py

--- generate_commit_dates.py
+++ generate_commit_dates.py
@@ -22,8 +22,8 @@ def generate_dates(start_date, text, intensity):
                 if start_date <= date < end_date:
                     # Add the date multiple times to create different intensities
                     if ascii_art[row][col] != ' ':
-                    for _ in range(10): # Add the date 10 times
-                        dates_to_commit.append(date)
+                        for _ in range(10): # Add the date 10 times
+                            dates_to_commit.append(date)
 
     
     return dates_to_commit
